2d9928b0021c68eabfbdd74f68745318910e2745
Anton V <anton@holomeeting.io>
2020-03-25 18:10:47 +0300

    add holoportator

---
+++ models/holoportator.py
@@ -0,0 +1,214 @@
+import os
+import torch
+import torch.nn.functional as F
+import numpy as np
+from tqdm import tqdm
+from .models import BaseModel
+from networks.networks import NetworksFactory, HumanModelRecovery
+from utils.nmr import SMPLRenderer
+from utils.detectors import PersonMaskRCNNDetector
+import utils.cv_utils as cv_utils
+import utils.util as util
+
+
+class Holoportator(BaseModel):
+    def __init__(self, opt):
+        super(Holoportator, self).__init__(opt)
+        self._name = 'Holoportator'
+
+        self._create_networks()
+
+        # prefetch variables
+        self.src_info = None
+        self.tsf_info = None
+        self.first_cam = None
+
+
+    def _create_networks(self):
+        # 1. create generator
+        self.generator = self._create_generator().cuda()
+
+        # 2. create hmr
+        self.hmr = self._create_hmr().cuda()
+
+        # 3. create render
+        self.render = SMPLRenderer(image_size=self._opt.image_size, tex_size=self._opt.tex_size,
+                                   has_front=self._opt.front_warp, fill_back=False).cuda()
+        # 4. pre-processor
+        if self._opt.has_detector:
+            self.detector = PersonMaskRCNNDetector(ks=self._opt.bg_ks, threshold=0.5, to_gpu=True)
+        else:
+            self.detector = None
+
+
+    def _create_generator(self):
+        net = NetworksFactory.get_by_name(self._opt.gen_name, src_dim=3+self._G_cond_nc,
+                                          tsf_dim=3+self._G_cond_nc, repeat_num=self._opt.repeat_num)
+
+        if self._opt.load_path:
+            self._load_params(net, self._opt.load_path)
+        elif self._opt.load_epoch > 0:
+            self._load_network(net, 'G', self._opt.load_epoch)
+        else:
+            raise ValueError('load_path {} is empty and load_epoch {} is 0'.format(
+                self._opt.load_path, self._opt.load_epoch))
+
+        net.eval()
+        return net
+
+
+    def _create_hmr(self):
+        hmr = HumanModelRecovery(self._opt.smpl_model)
+        saved_data = torch.load(self._opt.hmr_model)
+        hmr.load_state_dict(saved_data)
+        hmr.eval()
+        return hmr
+
+
+    @torch.no_grad()
+    def personalize(self, src_path, src_smpl=None, output_path=''):
+
+        ori_img = cv_utils.read_cv2_img(src_path)
+
+        # resize image and convert the color space from [0, 255] to [-1, 1]
+        img = cv_utils.transform_img(ori_img, self._opt.image_size, transpose=True) * 2 - 1.0
+        img = torch.tensor(img, dtype=torch.float32).cuda()[None, ...]
+
+        if src_smpl is None:
+            img_hmr = cv_utils.transform_img(ori_img, 224, transpose=True) * 2 - 1.0
+            img_hmr = torch.tensor(img_hmr, dtype=torch.float32).cuda()[None, ...]
+            src_smpl = self.hmr(img_hmr)
+        elif isinstance(src_smpl, np.ndarray):
+            src_smpl = torch.tensor(src_smpl, dtype=torch.float32).cuda().unsqueeze(0)
+
+        # source process, {'theta', 'cam', 'pose', 'shape', 'verts', 'j2d', 'j3d'}
+        src_info = self.hmr.get_details(src_smpl)
+        src_f2verts, src_fim, src_wim = self.render.render_fim_wim(src_info['cam'], src_info['verts'])
+        # src_f2pts = src_f2verts[:, :, :, 0:2]
+        src_info['fim'] = src_fim
+        src_info['wim'] = src_wim
+        src_info['cond'], _ = self.render.encode_fim(src_info['cam'], src_info['verts'], fim=src_fim, transpose=True)
+        src_info['f2verts'] = src_f2verts
+        src_info['p2verts'] = src_f2verts[:, :, :, 0:2]
+        src_info['p2verts'][:, :, :, 1] *= -1
+
+        if self._opt.only_vis:
+            src_info['p2verts'] = self.render.get_vis_f2pts(src_info['p2verts'], src_fim)
+        # add image to source info
+        src_info['img'] = img
+        src_info['image'] = ori_img
+
+        # 2. process the src inputs
+        if self.detector is not None:
+            bbox, body_mask = self.detector.inference(img[0])
+            bg_mask = 1 - body_mask
+        else:
+            # bg is 1, ft is 0
+            bg_mask = util.morph(src_info['cond'][:, -1:, :, :], ks=self._opt.bg_ks, mode='erode')
+            body_mask = 1 - bg_mask
+
+        ft_mask = 1 - util.morph(src_info['cond'][:, -1:, :, :], ks=self._opt.ft_ks, mode='erode')
+        src_inputs = torch.cat([img * ft_mask, src_info['cond']], dim=1)
+
+        src_info['feats'] = self.generator.encode_src(src_inputs)
+
+        self.src_info = src_info
+
+        if output_path:
+            cv_utils.save_cv2_img(src_info['image'], output_path, image_size=self._opt.image_size)
+
+
+    @torch.no_grad()
+    def inference_by_smpls(self, tgt_smpls, cam_strategy='smooth', output_dir=None):
+        length = len(tgt_smpls)
+
+        outputs = []
+        for t in tqdm(range(length)):
+            tgt_smpl = tgt_smpls[t] if tgt_smpls is not None else None
+
+            tsf_inputs = self.transfer_params_by_smpl(tgt_smpl, cam_strategy, t=t)
+            preds = self.forward(tsf_inputs, self.tsf_info['T'])
+
+            preds = preds[0].permute(1, 2, 0)
+            preds = preds.cpu().numpy()
+            outputs.append(preds)
+
+            if output_dir is not None:
+                cv_utils.save_cv2_img(preds, output_dir[t], normalize=True)
+
+        return outputs
+
+
+    def swap_smpl(self, src_cam, src_shape, tgt_smpl, cam_strategy='smooth'):
+        tgt_cam = tgt_smpl[:, 0:3].contiguous()
+        pose = tgt_smpl[:, 3:75].contiguous()
+
+        # TODO, need more tricky ways
+        if cam_strategy == 'smooth':
+
+            cam = src_cam.clone()
+            delta_xy = tgt_cam[:, 1:] - self.first_cam[:, 1:]
+            cam[:, 1:] += delta_xy
+
+        elif cam_strategy == 'source':
+            cam = src_cam
+        else:
+            cam = tgt_cam
+
+        tsf_smpl = torch.cat([cam, pose, src_shape], dim=1)
+
+        return tsf_smpl
+
+
+    def transfer_params_by_smpl(self, tgt_smpl, cam_strategy='smooth', t=0):
+        # get source info
+        src_info = self.src_info
+
+        if isinstance(tgt_smpl, np.ndarray):
+            tgt_smpl = torch.tensor(tgt_smpl).float().cuda()[None, ...]
+
+        if t == 0 and cam_strategy == 'smooth':
+            self.first_cam = tgt_smpl[:, 0:3].clone()
+
+        # get transfer smpl
+        tsf_smpl = self.swap_smpl(src_info['cam'], src_info['shape'], tgt_smpl, cam_strategy=cam_strategy)
+        # transfer process, {'theta', 'cam', 'pose', 'shape', 'verts', 'j2d', 'j3d'}
+        tsf_info = self.hmr.get_details(tsf_smpl)
+
+        tsf_f2verts, tsf_fim, tsf_wim = self.render.render_fim_wim(tsf_info['cam'], tsf_info['verts'])
+        # src_f2pts = src_f2verts[:, :, :, 0:2]
+        tsf_info['fim'] = tsf_fim
+        tsf_info['wim'] = tsf_wim
+        tsf_info['cond'], _ = self.render.encode_fim(tsf_info['cam'], tsf_info['verts'], fim=tsf_fim, transpose=True)
+        # tsf_info['sil'] = util.morph((tsf_fim != -1).float(), ks=self._opt.ft_ks, mode='dilate')
+
+        T = self.render.cal_bc_transform(src_info['p2verts'], tsf_fim, tsf_wim)
+        tsf_img = F.grid_sample(src_info['img'], T)
+        tsf_inputs = torch.cat([tsf_img, tsf_info['cond']], dim=1)
+
+        # add target image to tsf info
+        tsf_info['tsf_img'] = tsf_img
+        tsf_info['T'] = T
+
+        self.tsf_info = tsf_info
+
+        return tsf_inputs
+
+
+    def forward(self, tsf_inputs, T):
+        src_encoder_outs, src_resnet_outs = self.src_info['feats']
+
+        tsf_color, tsf_mask = self.generator.inference(src_encoder_outs, src_resnet_outs, tsf_inputs, T)
+        pred_imgs = (1 - tsf_mask) * tsf_color
+
+        if self._opt.front_warp:
+            pred_imgs = self.warp_front(pred_imgs, tsf_mask)
+
+        return pred_imgs
+
+
+    def warp_front(self, preds, mask):
+        front_mask = self.render.encode_front_fim(self.tsf_info['fim'], transpose=True, front_fn=True)
+        preds = (1 - front_mask) * preds + self.tsf_info['tsf_img'] * front_mask * (1 - mask)
+        # preds = torch.clamp(preds + self.tsf_info['tsf_img'] * front_mask, -1, 1)
+        return preds
---
+++ run_holoportator.py
@@ -0,0 +1,141 @@
+import os
+import numpy as np
+import torch
+from models.holoportator import Holoportator
+from options.test_options import TestOptions
+
+
+def get_file_paths(path, exts=('.jpeg','.jpg','.png')):
+    file_paths = []
+    for dirpath, dirnames, filenames in os.walk(path):
+        for filename in [f for f in filenames if any(f.endswith(ext) for ext in exts)]:
+            file_paths.append(os.path.join(dirpath,filename))
+            file_paths.sort()
+
+    return file_paths
+
+
+def prepare_test_data(frames_dir, smpls_dir):
+    # Parse frame paths:
+    subdirs = [p.path for p in os.scandir(frames_dir) if p.is_dir()]
+    subdirs.sort()
+    frame_paths = {}
+
+    for s_dir in subdirs:
+        cam_name = s_dir.split('/')[-1]
+        paths = get_file_paths(s_dir)
+        frame_paths[cam_name] = paths
+
+    # Load smpls:
+    subdirs = [p.path for p in os.scandir(smpls_dir) if p.is_dir()]
+    subdirs.sort()
+    smpls = {}
+
+    for s_dir in subdirs:
+        smpl_path = get_file_paths(s_dir, exts=('.npz'))[0]
+        with np.load(smpl_path, encoding='latin1', allow_pickle=True) as data:
+            smpl_data = dict(data)
+            n = len(smpl_data['cams'])
+            smpl_vecs = []
+
+            for frame_id in range(n):
+                cams = smpl_data['cams'][frame_id]
+                pose = smpl_data['pose'][frame_id]
+                shape = smpl_data['shape'][frame_id]
+                vec = np.concatenate((cams, pose, shape), axis=0)
+                vec = np.expand_dims(vec, axis=0)
+                smpl_vecs.append(torch.tensor(vec).float().cuda())
+
+            cam_name = s_dir.split('/')[-1]
+            smpls[cam_name] = torch.cat(smpl_vecs, dim=0)
+
+    # Check:
+    assert len(smpls) == len(frame_paths)
+    n = len(list(frame_paths.values())[0])
+    assert (n == len(v) for v in frame_paths.values())
+    m = list(smpls.values())[0].shape[0]
+    assert (m == smpl.shape[0] for smpl in smpls.values())
+    assert n == m
+    print ('Data has been loaded from path:', frames_dir)
+    print ('Total cams={}, frames={}'.format(len(smpls), n))
+
+    test_data = {
+        'frame_paths' : frame_paths,
+        'smpls' : smpls
+    }
+
+    return test_data, n
+
+
+def holoportate(test_opt):
+    # Load and prepare test data:
+    smpls_dir = os.path.join(test_opt.root_dir, test_opt.smpl_dir, test_opt.scene_path)
+    frames_dir = os.path.join(test_opt.root_dir, test_opt.frames_dir, test_opt.scene_path)
+    test_data, n = prepare_test_data(frames_dir, smpls_dir)
+
+    if test_opt.frames_range[1] < test_opt.frames_range[0]:
+        test_opt.frames_range = (test_opt.frames_range[0],n)
+
+    # Determine target camera names:
+    tgt_cams = []
+    for cam_name in test_data['frame_paths'].keys():
+        if cam_name != test_opt.src_cam:
+            tgt_cams.append(cam_name)
+
+    # Prepare output dir:
+    output_dir = os.path.join(test_opt.root_dir, test_opt.output_dir)
+    for cam_name in tgt_cams + [test_opt.src_cam]:
+        path = os.path.join(output_dir, cam_name)
+        if not os.path.exists(path):
+            os.makedirs(path)
+
+    # Init Holoportator:
+    holoport = Holoportator(test_opt)
+
+    for idx in range(test_opt.frames_range[0], test_opt.frames_range[1]):
+        print ('Processing frame', idx)
+
+        # Personalize model:
+        img_path = test_data['frame_paths'][test_opt.src_cam][idx]
+        smpl = test_data['smpls'][test_opt.src_cam][idx].unsqueeze(0)
+        out_path_src = os.path.join(output_dir, test_opt.src_cam, str(idx).zfill(5) + '.jpeg')
+        holoport.personalize(src_path=img_path, src_smpl=smpl, output_path=out_path_src)
+
+        # Inference:
+        tgt_smpls, out_paths = [], []
+        for cam_name in tgt_cams:
+            tgt_smpls.append(test_data['smpls'][cam_name][idx].unsqueeze(0))
+            out_paths.append(os.path.join(output_dir, cam_name, str(idx).zfill(5) + '.jpeg'))
+        holoport.inference_by_smpls(tgt_smpls=tgt_smpls, cam_strategy='', output_dir=out_paths)
+
+    print ('All done!')
+
+
+if __name__ == "__main__":
+    # Parse options:
+    test_opt = TestOptions().parse()
+
+    # Data:
+    test_opt.root_dir = '/home/darkalert/KazendiJob/Data/HoloVideo/Data'
+    test_opt.frames_dir = 'avatars'
+    test_opt.smpl_dir = 'smpl_aligned_lwgan'
+    test_opt.scene_path = 'person_2/light-100_temp-5600/garments_1/freestyle'
+    test_opt.output_dir = 'test/holoportator/p2_l100_g1_freestyle'
+    test_opt.src_cam = 'cam1'
+    test_opt.frames_range = (0,-1)
+
+    # Model:
+    test_opt.gpu = '0'
+    test_opt.gen_name = "holoportator"
+    test_opt.image_size = 256
+    test_opt.checkpoints_dir = './outputs/checkpoints'
+    test_opt.load_path = '/home/darkalert/builds/impersonator/outputs/Holo1/net_epoch_13_id_G.pth'
+    test_opt.bg_ks = 11
+    test_opt.ft_ks = 3
+    test_opt.has_detector = False
+    test_opt.post_tune = False
+    test_opt.front_warp = False
+    test_opt.save_res = True
+
+    holoportate(test_opt)
+
