a392b7138ce3bf7a23b16d6c83323ab1d715f37e
darkAlert <darkangel-nwo@yandex.ru>
2020-04-08 15:00:15 +0300

    init lwganrt

---
+++ setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name='lwganrt',
+    version='1.0.0',
+    description='Real-time Liquid Warping GAN: A Unified Framework for Human Motion Imitation, Appearance Transfer and Novel View Synthesis',
+    author='svip-lab,darkAlert',
+    packages=setuptools.find_packages()
+)
